420411bda9e15a262d60380e5be8a24302612e66
Jack Duggan <74904632+jackjduggan@users.noreply.github.com>
2024-03-07 15:08:09 +0000

    centralized styles, App.jsx cleanup

---
+++ react-amplified/src/config.js
@@ -0,0 +1,7 @@
+// src/config.js
+
+const config = {
+    API_ENDPOINT: 'http://192.168.181.132:5000',
+  };
+  
+  export default config;
--- react-amplified/vite.config.js
+++ react-amplified/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // added this to try get image to display - didn't work.
+  //base: "./", // added this to try get image to display - didn't work.
 })
